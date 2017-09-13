ae16e36979a41c63a4c85a551403ddc26e4f12f5
dastanaron <flow199@yandex.ru>
2017-09-13 23:17:29 +0300

    added new point on main menu

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -8,19 +8,28 @@ $this->title = 'Личная программа для разаработок';
 <div class="container">
 
     <div class="row">
-        <div class="col-sm-6">
+        <div class="col-sm-4">
             <a href="/funds" class="btn btn-info center-block">Движение денежных средств</a>
         </div>
-        <div class="col-sm-6">
-            <?php
-            echo Html::beginForm(['/site/logout'], 'post');
-            echo Html::submitButton(
-                'Выход (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-primary center-block']
-            );
-            echo Html::endForm();
-            ?>
+
+        <div class="col-sm-4">
+            <a href="/events" class="btn btn-info center-block">Напоминания</a>
         </div>
+
+        <div class="col-sm-4">
+            <a href="/" class="btn btn-info center-block">в разработке</a>
+        </div>
+    </div>
+
+    <div class="exit margin-top-80">
+        <?php
+        echo Html::beginForm(['/site/logout'], 'post');
+        echo Html::submitButton(
+            'Выход (' . Yii::$app->user->identity->username . ')',
+            ['class' => 'btn btn-primary center-block']
+        );
+        echo Html::endForm();
+        ?>
     </div>
 
 </div>
